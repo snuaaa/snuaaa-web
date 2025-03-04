3c4f0c2ca9bbc99e8f1c0b74e59a368400acfafb
YoungJae Kim <yjkim1803@gmail.com>
2025-03-05 02:04:04 +0900

    refactor: EquipmentContext 사용하여 props 개선

--- packages/community/src/components/Equipment/Rent.tsx
+++ packages/community/src/components/Equipment/Rent.tsx
@@ -1,54 +1,19 @@
-import { useFetch } from 'hooks/useFetch';
-import { FC, useCallback, useState } from 'react';
-import EquipmentService from 'services/EquipmentService';
+import { FC } from 'react';
 import EquipSearchBar from './EquipSearchBar';
 import EquipList from './EquipList';
 import Loading from 'components/Common/Loading';
 import { withModal } from 'contexts/modal';
 import BoardName from 'components/Board/BoardName';
-import { Equipment } from 'services/types';
 import EquipCart from './EquipCart';
 import { Link } from 'react-router-dom';
+import { useEquipment, withEquipment } from './contexts';
 
 const Rent: FC = () => {
-  const fetchFunction = useCallback(() => {
-    return EquipmentService.retrieveList(1);
-  }, []);
-
-  const { data, refresh } = useFetch({ fetch: fetchFunction });
-  const [cart, setCart] = useState<Equipment[]>([]);
+  const { data } = useEquipment();
 
   if (!data) {
     return <Loading />;
   }
-
-  const handleClickEquipmentRent = (equipment: Equipment) => {
-    window.confirm('이 장비를 대여하시겠습니까?') &&
-      EquipmentService.rentEquipments({ equipmentIds: [equipment.id] }).then(
-        () => {
-          refresh();
-          setCart(cart.filter((equip: Equipment) => equip.id !== equipment.id));
-        },
-      );
-  };
-
-  const handleClickEquipmentCart = (equipment: Equipment) => {
-    if (cart.find((equip: Equipment) => equip.id === equipment.id)) {
-      setCart(cart.filter((equip: Equipment) => equip.id !== equipment.id));
-    } else {
-      setCart([...cart, equipment]);
-    }
-  };
-
-  const handleClickRentAll = () => {
-    window.confirm(`총 ${cart.length}개의 장비를 대여하시겠습니까?`) &&
-      EquipmentService.rentEquipments({
-        equipmentIds: cart.map((equip) => equip.id),
-      }).then(() => {
-        refresh();
-        setCart([]);
-      });
-  };
 
   return (
     <div className="board-wrapper">
@@ -62,24 +27,14 @@ const Rent: FC = () => {
       <EquipSearchBar />
       <div className="w-full flex">
         <span className="w-2/3 overflow-y-auto h-screen">
-          <EquipList
-            onClickEquipmentRent={handleClickEquipmentRent}
-            onClickEquipmentCart={handleClickEquipmentCart}
-            data={data}
-            columns={2}
-            cart={cart}
-          />
+          <EquipList data={data} columns={2} type="rent" />
         </span>
         <span className="w-1/3 overflow-y-auto h-screen">
-          <EquipCart
-            equipments={cart}
-            onClickCancel={handleClickEquipmentCart}
-            onClickRentAll={handleClickRentAll}
-          />
+          <EquipCart />
         </span>
       </div>
     </div>
   );
 };
 
-export default withModal(Rent);
+export default withModal(withEquipment(Rent));
