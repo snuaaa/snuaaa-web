b19e07eb5260af6e9f5db0ef7ee3af74eca56b1a
Seungmin Choi <seungmin960@gmail.com>
2025-03-23 18:16:12 +0900

    apply line-clamp-2 to long equip names(#86)

--- packages/community/src/components/Equipment/EquipCart/CartItem.tsx
+++ packages/community/src/components/Equipment/EquipCart/CartItem.tsx
@@ -12,7 +12,7 @@ const CartItem: FC<Props> = ({ equip, onClickCancel, columns }) => {
   return (
     <div
       className={
-        (columns === 1 ? 'w-full' : 'w-1/' + columns.toString()) + ' h-24 flex'
+        (columns === 1 ? 'w-full' : 'w-1/' + columns.toString()) + ' h-30 flex'
       }
       key={equip.id}
     >
@@ -23,7 +23,7 @@ const CartItem: FC<Props> = ({ equip, onClickCancel, columns }) => {
             className="max-h-16 object-contain max-w-full mx-auto"
           />
         </div>
-        <div className="text-xs font-bold my-1">{equip.name}</div>
+        <div className="text-xs font-bold my-1 line-clamp-2">{equip.name}</div>
         <div className="z-1 absolute top-0 right-0 bg-cyan-600 text-white text-xs font-bold px-1">
           <button onClick={onClickCancel}>
             <i className="ri-close-line text-xl"></i>
