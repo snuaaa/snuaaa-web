4ad7710eb4223cace0f34429686af0be410034c5
Enif <yjkim1803@gmail.com>
2025-05-01 23:28:59 +0900

    fix: asset path

--- packages/community/src/components/Header/index.tsx
+++ packages/community/src/components/Header/index.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
-import logo from '../assets/img/logo_white.png';
-import imgProfile from '../assets/img/common/profile.png';
+import logo from '~/assets/img/logo_white.png';
+import imgProfile from '~/assets/img/common/profile.png';
 import Navigation from '~/components/Header/Navigation';
 import PopupUser from '~/components/Header/PopupUser';
 import Image from '~/components/Common/AaaImage';
 import { useHistory } from 'react-router';
 import { useAuth } from '~/contexts/auth';
 import { useBoards } from '~/contexts/board';
-import backgroundImg from '../assets/img/header.gif';
+import backgroundImg from '~/assets/img/header.gif';
 
 function Header() {
   const [isShowPopupUser, setIsShowPopupUser] = useState(false);
