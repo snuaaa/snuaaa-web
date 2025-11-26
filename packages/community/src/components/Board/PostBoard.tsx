58f184efd08513ff7b3202fc6e7eef61366f3cb5
YoungJae Kim <yjkim1803@gmail.com>
2025-11-26 23:05:33 +0900

    feat: post list에 pagination 적용

--- packages/community/src/components/Board/PostBoard.tsx
+++ packages/community/src/components/Board/PostBoard.tsx
@@ -8,9 +8,7 @@ import {
 
 import Loading from '../../components/Common/Loading';
 import PostList from '../../components/Post/PostList';
-import Paginator from '../../components/Common/Paginator';
 import BoardName from '../../components/Board/BoardName';
-import SearchTypeEnum from '../../common/SearchTypeEnum';
 import SelectBox from '../../components/Common/SelectBox';
 
 import { useLocation, useHistory } from 'react-router';
@@ -19,23 +17,26 @@ import PostService from '~/services/PostService';
 import { useFetch } from '~/hooks/useFetch';
 import CreatePost from '~/components/Post/CreatePost';
 import { useAuth } from '~/contexts/auth';
+import useQueryString from '~/hooks/useQueryString';
+import Pagination from '../Common/Pagination';
 
-const POSTROWNUM = 10;
+const PAGE_SIZE = 10;
+
 const searchOptions = [
   {
-    id: SearchTypeEnum.ALL,
+    id: 'all',
     name: '제목+내용',
   },
   {
-    id: SearchTypeEnum.TITLE,
+    id: 'title',
     name: '제목',
   },
   {
-    id: SearchTypeEnum.TEXT,
+    id: 'text',
     name: '내용',
   },
   {
-    id: SearchTypeEnum.USER,
+    id: 'user',
     name: '글쓴이',
   },
 ];
@@ -44,84 +45,59 @@ type PostBoardProps = {
   boardInfo: Board;
 };
 
-type LocationState = {
-  page: number;
-  searchInfo: {
-    type: string;
-    keyword: string;
-  };
-};
-
 function PostBoard({ boardInfo }: PostBoardProps) {
-  const location = useLocation<LocationState>();
+  const location = useLocation();
   const history = useHistory();
-  const [searchInfo, setSearchInfo] = useState<{
-    type: string;
-    keyword: string;
-  }>({
-    type: SearchTypeEnum.ALL,
-    keyword: '',
-  });
+
+  const queryString = useQueryString();
+  const page = Number(queryString.get('page') ?? 1);
 
   const [isCreating, setIsCreating] = useState(false);
-
-  const pageIdx = location.state?.page ?? 1;
+  const [searchKeyword, setSearchKeyword] = useState(
+    queryString.get('search_keyword') || '',
+  );
+  const [searchType, setSearchType] = useState(
+    queryString.get('search_type') || '',
+  );
 
   const authContext = useAuth();
 
   const fetchFunction = useCallback(async () => {
-    const searchInfo = location.state && location.state.searchInfo;
-
-    return searchInfo && searchInfo.keyword
-      ? PostService.searchPostsInBoard(
-          boardInfo.board_id,
-          searchInfo.type,
-          searchInfo.keyword,
-          pageIdx,
-        )
-      : PostService.retrievePostsInBoard(boardInfo.board_id, pageIdx);
-  }, [boardInfo.board_id, location.state, pageIdx]);
+    return PostService.retrievePostList({
+      board_id: boardInfo.board_id,
+      search_type: queryString.get('type') || undefined,
+      search_keyword: queryString.get('keyword') || undefined,
+      offset: (page - 1) * PAGE_SIZE,
+      limit: PAGE_SIZE,
+    });
+  }, [boardInfo.board_id, page, queryString]);
 
   const { data, refresh } = useFetch({ fetch: fetchFunction });
 
-  useEffect(() => {
-    if (location.state && location.state.searchInfo) {
-      setSearchInfo(location.state.searchInfo);
-    }
-  }, [location.state]);
-
+  // TODO: 게시글 작성시 route 변경하여 불필요한 useEffect 제거
   useEffect(() => {
     setIsCreating(false);
   }, [boardInfo.board_id]);
 
-  const clickPage = (idx: number) => {
-    history.push({
-      state: {
-        ...location.state,
-        page: idx,
-      },
-    });
-  };
-
   const handleSearchOption = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchInfo({
-      ...searchInfo,
-      type: e.target.value,
-    });
+    setSearchType(e.target.value);
   };
 
   const search = async () => {
-    if (!searchInfo || !searchInfo.keyword || searchInfo.keyword.length < 2) {
-      alert('2글자 이상 입력해주세요.');
-    } else {
-      history.push({
-        state: {
-          ...location.state,
-          page: 1,
-          searchInfo: searchInfo,
-        },
-      });
+    if (!searchType) {
+      const nextSearchParam = new URLSearchParams();
+      history.push({ search: nextSearchParam.toString() });
+      return;
     }
+    if (searchKeyword.trim().length < 2) {
+      alert('2글자 이상 입력해주세요.');
+      return;
+    }
+    const nextSearchParam = new URLSearchParams(queryString);
+    nextSearchParam.set('keyword', searchKeyword.trim());
+    nextSearchParam.set('type', searchType);
+    nextSearchParam.set('page', '1');
+    history.push({ search: nextSearchParam.toString() });
   };
 
   const handleSearchKeyDown = (e: KeyboardEvent) => {
@@ -131,10 +107,7 @@ function PostBoard({ boardInfo }: PostBoardProps) {
   };
 
   const handleSearchKeyword = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchInfo({
-      ...searchInfo,
-      keyword: e.target.value,
-    });
+    setSearchKeyword(e.target.value);
   };
 
   const makeCategoryList = () => {
@@ -169,14 +142,14 @@ function PostBoard({ boardInfo }: PostBoardProps) {
                 selectName={'searchOption'}
                 optionList={searchOptions}
                 onSelect={handleSearchOption}
-                selectedOption={searchInfo.type}
+                selectedOption={searchType}
               />
             </div>
             <div className="board-search-input">
               <input
                 type="text"
                 onChange={handleSearchKeyword}
-                value={searchInfo.keyword}
+                value={searchKeyword}
                 onKeyDown={handleSearchKeyDown}
               />
               <button className="board-search-btn" onClick={search}>
@@ -195,12 +168,16 @@ function PostBoard({ boardInfo }: PostBoardProps) {
             </div>
           </div>
 
-          <PostList posts={data.postInfo} />
-          {data.postCount > 0 && (
-            <Paginator
-              pageIdx={pageIdx}
-              pageNum={Math.ceil(data.postCount / POSTROWNUM)}
-              clickPage={clickPage}
+          <PostList posts={data.rows} />
+          {data.count > 0 && (
+            <Pagination
+              currentPage={page}
+              totalPageCount={Math.ceil(data.count / PAGE_SIZE)}
+              routeGenerator={(page) => {
+                const nextSearchParam = new URLSearchParams(queryString);
+                nextSearchParam.set('page', page.toString());
+                return `${location.pathname}?${nextSearchParam.toString()}`;
+              }}
             />
           )}
         </>
