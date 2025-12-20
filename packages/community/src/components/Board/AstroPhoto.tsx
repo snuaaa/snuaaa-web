74c5a31ec7c6368f1ccd65855ecd2b2b259ebd28
YoungJae Kim <yjkim1803@gmail.com>
2025-12-20 19:10:40 +0900

    refactor: use button tag and tailwindcss in AstroPhoto component

--- packages/community/src/components/Board/AstroPhoto.tsx
+++ packages/community/src/components/Board/AstroPhoto.tsx
@@ -1,6 +1,7 @@
 import BoardName from '../../components/Board/BoardName';
 
 import { useLocation, useHistory } from 'react-router';
+import useQueryString from '~/hooks/useQueryString';
 import { Board } from '~/services/types';
 import { PhotoSection } from './PhotoBoard/PhotoSection';
 import { AlbumSection } from './PhotoBoard/AlbumSection';
@@ -9,32 +10,37 @@ type AstroPhotoProps = {
   boardInfo: Board;
 };
 
-type LocationState = {
-  page: number;
-  isViewAlbums: boolean;
-  tags: string[];
-};
+type ViewMode = 'photo' | 'album';
+
+const baseViewModeClassName =
+  'w-1/2 text-center py-[10px] transition-all duration-500';
+
+const selectedViewModeClassName =
+  'bg-[#49A0AE] text-white';
+
+const unselectedViewModeClassName =
+  'bg-[#F1F1F1] text-[#A3A3A3] hover:text-[#646464]';
 
 function AstroPhoto({ boardInfo }: AstroPhotoProps) {
   const history = useHistory();
-  const location = useLocation<LocationState>();
+  const queryString = useQueryString();
+  const location = useLocation();
 
-  const setIsViewAlbums = (isViewAlbums: boolean) => {
-    history.replace({
-      state: {
-        isViewAlbums: isViewAlbums,
-        page: 1,
-      },
+  const viewMode: ViewMode =
+    queryString.get('view') === 'album' ? 'album' : 'photo';
+
+  const setViewMode = (mode: ViewMode) => {
+    const nextSearchParams = new URLSearchParams(queryString);
+    nextSearchParams.set('view', mode);
+    nextSearchParams.set('page', '1'); // Reset page when switching views
+
+    history.push({
+      pathname: location.pathname,
+      search: nextSearchParams.toString(),
     });
   };
 
-  const isViewAlbums =
-    location.state && location.state.isViewAlbums ? true : false;
 
-  const albumSelectorClassName =
-    'view-type-selector' + (isViewAlbums ? '' : ' selected');
-  const photoSelectorClassName =
-    'view-type-selector' + (isViewAlbums ? ' selected' : '');
 
   return (
     <div className="board-wrapper photoboard-wrapper">
@@ -43,21 +49,29 @@ function AstroPhoto({ boardInfo }: AstroPhotoProps) {
         board_name={boardInfo.board_name}
       />
       <div className="board-desc">{boardInfo.board_desc}</div>
-      <div className="view-type-selector-wrapper">
-        <div
-          className={albumSelectorClassName}
-          onClick={() => setIsViewAlbums(false)}
+      <div className="w-full flex text-[20px] mb-3">
+        <button
+          className={`${baseViewModeClassName} ${
+            viewMode === 'photo'
+              ? selectedViewModeClassName
+              : unselectedViewModeClassName
+          }`}
+          onClick={() => setViewMode('photo')}
         >
           사진
-        </div>
-        <div
-          className={photoSelectorClassName}
-          onClick={() => setIsViewAlbums(true)}
+        </button>
+        <button
+          className={`${baseViewModeClassName} ${
+            viewMode === 'album'
+              ? selectedViewModeClassName
+              : unselectedViewModeClassName
+          }`}
+          onClick={() => setViewMode('album')}
         >
           앨범
-        </div>
+        </button>
       </div>
-      {!isViewAlbums ? (
+      {viewMode === 'photo' ? (
         <PhotoSection boardInfo={boardInfo} />
       ) : (
         <AlbumSection boardInfo={boardInfo} />
