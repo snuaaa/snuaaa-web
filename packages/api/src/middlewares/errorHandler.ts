9b4537dd0f7729c5a587b0ec1c5aab068892ffd6
YoungJae Kim <yjkim1803@gmail.com>
2026-06-29 23:11:16 +0100

    [feat] setup auto deploy (#132)
    * 👷 fix Docker configuration
    
    * 👷 add GitHub Actions workflow for API deployment
    
    * 🐛 fix docker container runtime
    
    * 🐛 fixes the issue that the error handler didn't work as a  error handler middleware

--- packages/api/src/middlewares/errorHandler.ts
+++ packages/api/src/middlewares/errorHandler.ts
@@ -1,5 +1,6 @@
 // error handler middleware
-export function errorHandler(err, req, res) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function errorHandler(err, req, res, _next) {
   console.error(JSON.stringify(err));
   res.status(err.status || 500).json({
     success: false,
